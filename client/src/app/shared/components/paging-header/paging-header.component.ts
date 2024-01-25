28840d1f1c77cc939047f52d49246fc8f5a39fe6
Mozaffer <mozafer_55@hotmail.com>
2024-01-25 22:05:27 +0300

    Page Header & API Request

---
+++ client/src/app/shared/components/paging-header/paging-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paging-header',
+  templateUrl: './paging-header.component.html',
+  styleUrls: ['./paging-header.component.scss']
+})
+export class PagingHeaderComponent {
+
+}
---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
+
+
+
+@NgModule({
+  declarations: [
+    PagingHeaderComponent,
+    PagerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
