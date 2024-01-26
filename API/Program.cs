0e21445920e8a76eba0d5fae07d6cee46bdc05e9
Mozaffer <mozafer_55@hotmail.com>
2024-01-26 12:17:12 +0300

    api enhance validation

--- API/Program.cs
+++ API/Program.cs
@@ -16,12 +16,16 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
+builder.Services.AddSwaggerDocumentation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+app.UseSwaggerDocumentation();
 
 app.UseSwagger();
 app.UseSwaggerUI();
